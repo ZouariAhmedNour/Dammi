691430af14d1e3b6d0533c607fc6ea8ff202c772
ZouariAhmedNour <zouariahmednour@gmail.com>
2026-04-03 14:54:03 +0100

    Initial commit with backend and frontend

---
+++ backend/dammi/src/main/java/com/example/dammi/DammiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dammi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DammiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DammiApplication.class, args);
+    }
+
+}
---
+++ backend/dammi/src/test/java/com/example/dammi/DammiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dammi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DammiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
